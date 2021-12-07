e02d33b2a2c95d4115275b2e7a02c9cc11645614
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2021-12-07 12:54:55 +0800

    commit

--- Program1(PLD8).py
+++ Program1(PLD8).py
@@ -11,31 +11,33 @@
 
 import random
 
-def Wnumbers():
-    intp1 = int(input("Enter your First number: "))
-    intp2 = int(input("Enter your Second number: "))
-    intp3 = int(input("Enter your Third number: "))
+print("\n >>\33[33;1mLOTTERY\33[0m<<")
+play = True
+while play:
+    print("\n\33[32;1mJust Enter three digit numbers from 0 to 9\33[0m")
+    intp1 = int(input("\n\33[31;1mEnter your First number\33[0m: "))
+    intp2 = int(input("\33[31;1mEnter your Second number\33[0m: "))
+    intp3 = int(input("\33[31;1mEnter your Third number\33[0m: "))
 
     num1 = random.randint(0,9)
     num2 = random.randint(0,9)
     num3 = random.randint(0,9)
-    user = (intp1, intp2, intp3)
-    win = (num1, num2, num3)
+    user = intp1, intp2, intp3
     lose = (num1, num2, num3)
-    if  user == win:
-        print("You, Win")
-        print(win)
+    if  user == num1 and num2 and num3:
+        print("\33[33;1mWinner\33[0m")
+        print(lose)
     else:
-        print("You, Lose")
-        print(f"Lottery Number are {lose}.")
+        print(f"\33[36;1mWinning Number are \33[0m{lose}.")        
+        print("\33[33;4mYou Loss\33[0m")
+        print(f"\33[32;1mYour input Number are\33[0m {user}")
 
-def Play():
-    play = input("You want to continue Y/N?\n")
-    if play == ("Y") or ("y") or ("YES") or ("Yes") or ("yes"):
-        print(Wnumbers())
-        print(Play(),Play(),Play(),Play(),Play(),Play(),Play(),Play(),Play(),Play(),Play())
-    else:
-        print("Done")
-Wnumbers()
-Play()
-
+    ans = input("\n\33[34;1mYou want to continue Yes or No\33[0m?:\n").lower()
+    while True:
+        if ans == "no":
+            print("\33[35;1mThanks for playing\33[0m")
+            play = False
+            break
+        elif ans == "yes":
+            play = True
+            break
